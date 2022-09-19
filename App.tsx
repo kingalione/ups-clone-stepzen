10d83b444d87344f93e410ce1325af0cf4d0d041
akaradag <a.karadag@5flow.eu>
2022-09-19 22:19:58 +0200

    adding apollo client and hooks
    - add apollo client
    - add useful hooks
    - add graphql queries

--- App.tsx
+++ App.tsx
@@ -1,18 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {TailwindProvider} from 'tailwind-rn';
+import { TailwindProvider } from 'tailwind-rn';
 import CustomerScreen from './screens/CustomersScreen';
 import utilities from './tailwind.json';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './navigator/RootNavigator';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5001/api/dusty-gopher',
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
     // @ts-ignore - TailwindProvider is missing a type definition
     <TailwindProvider utilities={utilities}>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </ApolloProvider>
     </TailwindProvider>
   );
 }
--- typings.d.ts
+++ typings.d.ts
@@ -7,3 +7,37 @@ type CustomerList = {
     name: ID;
     value: Customer;
 }
+
+type TrackingItem = {
+    customer_id: String
+    items: [Items]
+    customer: Customer
+}
+
+type Item = {
+    item_id: Int
+    name: String
+    price: Float
+    quantity: Int
+}
+
+type OrderResponse = {
+    value: Order
+}
+
+type CustomerResponse = {
+    name: ID
+    value: Customer
+}
+
+type Order = {
+    carrier: String,
+    createdAt: Date,
+    shippingCost: Int,
+    trackingId: String,
+    Address: String,
+    City: String,
+    Lat: Float,
+    Lng: Float,
+    trackingItems: TrackingItems
+}
